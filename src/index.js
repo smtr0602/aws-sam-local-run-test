3f221fcfec21e94fa01576911deef5e3bae87c4b
Ryo Somatani <ryo_somatani@outlook.com>
2023-04-03 16:53:31 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event, context) => {
+  console.log('******** Running with SAM... ********');
+  console.log('Body:', JSON.stringify(event.body));
+  console.log('**************************************');
+
+  return {
+    statusCode: 200,
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+};
+
+// Test lambda (event.json used)
+// ----------------------------------------------------
+// $ sam local invoke -e ./src/event.json DemoFunction
+// ----------------------------------------------------
+
+// Test lambda with API (event.json not used)
+// ----------------------------------------------------
+// $ sam local start-api
+// ----------------------------------------------------
